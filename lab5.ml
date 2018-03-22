5500653c93cd1ebb567628834126ca7a5200195f
zpf0917 <31633332+zpf0917@users.noreply.github.com>
2018-03-22 15:55:02 -0400

    lll

--- lab5.ml
+++ lab5.ml
@@ -3,7 +3,7 @@
                Imperative Programming and References
                              Spring 2018
  *)
-   
+
 (*
 Objective:
 
@@ -21,15 +21,16 @@ of incrementing the integer stored in the ref. What is an appropriate
 type for the return value? What should the type for the function as a
 whole be? *)
 
-   
+
 (* Now implement the function. (As usual, for this and succeeding
 exercises, you shouldn't feel beholden to how the definition is
 introduced in the skeleton code below. For instance, you might want to
 add a "rec", or use a different argument list, or no argument list at
 all but binding to an anonymous function instead.) *)
 
-let inc _ =
-  failwith "inc not implemented" ;;
+let inc (x: int ref): unit =
+  x := !x +1;;
+
 
 (* Write a function named remember that returns the last string that
 it was called with. The first time it is called, it should return the
@@ -47,8 +48,12 @@ This is probably the least functional function ever written.
 As usual, you shouldn't feel beholden to how the definition is
 introduced in the skeleton code below. *)
 
-let remember _ = 
-  failwith "remember not implemented" ;;
+let remember: string -> string =
+  let ctr = ref "" in
+  fun (x: string) ->
+    let ctr1 = !ctr in
+    ctr := x;
+    ctr1;;
 
 (*====================================================================
 Part 2: Gensym
@@ -90,16 +95,19 @@ Complete the implementation of gensym. As usual, you shouldn't feel
 beholden to how the definition is introduced in the skeleton code
 below. (We'll stop mentioning this now.) *)
 
-let gensym (s : string) : string = 
-  failwith "gensym not implemented" ;;
+let gensym: string -> string =
+  let ctr = ref (-1) in
+  fun (s: string) ->
+    ctr := !ctr + 1;
+    s ^ (string_of_int !ctr);;
 
 (*====================================================================
 Part 3: Appending mutable lists
 
 Recall the definition of the mutable list type from lecture: *)
 
-type 'a mlist = 
-  | Nil 
+type 'a mlist =
+  | Nil
   | Cons of 'a * ('a mlist ref) ;;
 
 (* Mutable lists are just like regular lists, except that the tail of
@@ -119,8 +127,10 @@ list to a mutable list, with behavior like this:
       Cons (1, {contents = Cons (2, {contents = Cons (3, {contents = Nil})})})
  *)
 
-let mlist_of_list (lst : 'a list) : 'a mlist =
-  failwith "mlist_of_list not implemented" ;;
+let rec mlist_of_list (lst : 'a list) : 'a mlist =
+  match lst with
+  | [] -> Nil
+  | h :: t -> Cons (h , ref (mlist_of_list t));;
 
 (* Define a function length to compute the length of an mlist. Try to
 do this without looking at the solution that is given in the lecture
@@ -132,12 +142,15 @@ slides.
     - : int = 4
  *)
 
-let length (m : 'a mlist) : int = 
-  failwith "length not implemented" ;;
+let rec length (m : 'a mlist) : int =
+  match m with
+  | Nil -> 0
+  | Cons(_, a) -> 1 + length !a;;
 
 (* What is the time complexity of the length function in O() notation
 in terms of the length of its list argument? *)
 
+(* O(n) *)
 
 (* Now, define a function mappend that takes a *non-empty* mutable
 list and a second mutable list and, as a side effect, causes the first
@@ -148,7 +161,7 @@ about before you get started:
    can glean our intended answer from the examples below, but try to
    think it through yourself first.)
 
-#ifdef SOLN 
+#ifdef SOLN
 (* ANSWER: Since mappend is called for its side effect, unit is the
 appropriate return type. An alternative would be to return some
 indication as to whether the appending succeeded -- since it could
@@ -204,8 +217,17 @@ Example of use:
               {contents = Cons (5, {contents = Cons (6, {contents = Nil})})})})})})
  *)
 
-let mappend _ = 
-  failwith "mappend not implemented" ;;
+let mappend: 'a mlist -> 'a mlist -> unit =
+  fun (a: 'a mlist) (b:'a mlist) ->
+    let rec support a =
+      (match a with
+      | Nil -> failwith "lol"
+      | Cons(x,y)->
+        match !y with
+        | Nil -> y
+        | _ -> support !y)
+    in
+    (support a) := b;;
 
 (* What happens when you evaluate the following expressions
 sequentially in order?
@@ -278,25 +300,25 @@ module MakeImpQueue (A : sig
     type elt = A.t
     type mlist = Nil | Cons of elt * (mlist ref)
     type queue = {front: mlist ref ; rear: mlist ref}
-    let empty () = {front = ref Nil; rear = ref Nil} 
-    let enq x q = 
+    let empty () = {front = ref Nil; rear = ref Nil}
+    let enq x q =
       match !(q.rear) with
       | Cons (_h, t) -> assert (!t = Nil);
                         t := Cons(x, ref Nil);
                         q.rear := !t
-      | Nil -> assert (!(q.front) = Nil); 
+      | Nil -> assert (!(q.front) = Nil);
                q.front := Cons(x, ref Nil);
                q.rear := !(q.front)
-    let deq q = 
+    let deq q =
       match !(q.front) with
-      | Cons (h, t) -> 
+      | Cons (h, t) ->
          q.front := !t ;
          (match !t with
           | Nil -> q.rear := Nil
-          | Cons(_, _) -> ()); 
+          | Cons(_, _) -> ());
          Some h
       | Nil -> None
-    let to_string q = 
+    let to_string q =
       failwith "to_string not implemented"
   end ;;
 
@@ -313,7 +335,7 @@ module IntQueue = MakeImpQueue (struct
 the queue to a string to make sure that the right elements are in
 there. *)
 
-let test () = 
+let test () =
   let open IntQueue in
   let q = empty () in
   enq 1 q;
@@ -321,7 +343,7 @@ let test () =
   enq 3 q;
   to_string q ;;
 
-(* Running the test function should have the following behavior: 
+(* Running the test function should have the following behavior:
 
 # test () ;;
 - : string = "1 -> 2 -> 3 -> ||"
